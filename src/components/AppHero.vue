c70a37efda69f80dc220996d6ec9825040fff339
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-05-17 17:22:43 +0200

    add: vue and vite and completed exercise, bonus

---
+++ src/components/AppHero.vue
@@ -0,0 +1,10 @@
+<script>
+</script>
+
+<template>
+   <h2>Jumbotron</h2>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './AppTitle.vue'
+
+createApp(App).mount('#app')
